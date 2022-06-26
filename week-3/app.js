2129d8406dfa78726492d881d2dd94b4133f1ac3
lisa <lisalisa12332180@gmail.com>
2022-06-26 08:57:13 +0800

    0626_v1

--- week-3/app.js
+++ week-3/app.js
@@ -1,17 +1,17 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-  res.send("Hello, My Server!");
+app.get('/', function (req, res) {
+  res.send('Hello, My Server!');
 });
 
 var login = function (req, res, next) {
   let number = req.query.number;
   let _url = req.url;
-  if (_url == "/getData") {
+  if (_url == '/getData') {
     next();
-  } else if (_url !== "/getData" && number > 0) {
+  } else if (_url !== '/getData' && number > 0) {
     let sum = 0;
     for (let i = 0; i <= number; i++) {
       sum = sum + i;
@@ -22,8 +22,8 @@ var login = function (req, res, next) {
   }
 };
 
-app.get("/getData", login, function (req, res) {
-  res.send("Lack of Parameter");
+app.get('/getData', login, function (req, res) {
+  res.send('Lack of Parameter');
 });
 
 //監聽port
