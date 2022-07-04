81d0c049d8f0ca6f7ee4fbae516cf5e7a1c09195
彭璿 <pengxuan@Hsuans-MacBook-Pro-16-inch.local>
2022-07-04 11:12:26 +0800

    0704_v1

--- week-4/Assignment 3/index.js
+++ week-4/Assignment 3/index.js
@@ -1,23 +1,32 @@
 function Header() {
-
   const show = () => {
-    const aside = document.querySelector('.aside')
-    aside.style.display = "block";
-  }
+    const aside = document.querySelector('.aside');
+    aside.style.display = 'block';
+  };
 
   const close = () => {
-    const aside = document.querySelector('.aside')
+    const aside = document.querySelector('.aside');
     aside.style.display = 'none';
-  }
+  };
 
   return (
     <header>
       <ul className="aside">
-        <a href="#" className="close-menu" onClick={close}>X</a>
-        <li><a href="#">預約試駕</a></li>
-        <li><a href="#">購車資訊</a></li>
-        <li><a href="#">車主專區</a></li>
-        <li><a href="#">了解更多</a></li>
+        <a href="#" className="close-menu" onClick={close}>
+          X
+        </a>
+        <li>
+          <a href="#">預約試駕</a>
+        </li>
+        <li>
+          <a href="#">購車資訊</a>
+        </li>
+        <li>
+          <a href="#">車主專區</a>
+        </li>
+        <li>
+          <a href="#">了解更多</a>
+        </li>
       </ul>
       <nav>
         <div className="title">
@@ -28,27 +37,34 @@ function Header() {
           <img src="pic/menu icon.png" alt="menu-icon" />
         </div>
         <ul className="menu">
-          <li><a href="#">預約試駕</a></li>
-          <li><a href="#">購車資訊</a></li>
-          <li><a href="#">車主專區</a></li>
-          <li><a href="#">了解更多</a></li>
+          <li>
+            <a href="#">預約試駕</a>
+          </li>
+          <li>
+            <a href="#">購車資訊</a>
+          </li>
+          <li>
+            <a href="#">車主專區</a>
+          </li>
+          <li>
+            <a href="#">了解更多</a>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
 
 function Main() {
-
   const welcome = () => {
-    const h2 = document.querySelector('.welcome h2')
-    h2.textContent = 'Have a Good Time!'
-  }
+    const h2 = document.querySelector('.welcome h2');
+    h2.textContent = 'Have a Good Time!';
+  };
 
   const moreContent = () => {
     const content2 = document.querySelector('.content2');
     content2.style.display = 'block';
-  }
+  };
 
   return (
     <main>
@@ -64,28 +80,35 @@ function Main() {
         <ul className="content-list">
           <li>
             <p>S60 建議售價$1,845,000起</p>
-            <a href="#"><img src="pic/S60.jpeg" alt="S60" /></a>
+            <a href="#">
+              <img src="pic/S60.jpeg" alt="S60" />
+            </a>
           </li>
           <li>
             <p>XC60 建議售價$2,390,000起</p>
-            <a href="#"><img src="pic/XC60.jpeg" alt="S90" /></a>
+            <a href="#">
+              <img src="pic/XC60.jpeg" alt="S90" />
+            </a>
           </li>
           <li>
             <p>XC40 建議售價$1,607,000起</p>
-            <a href="#"><img src="pic/XC40.jpeg" alt="XC40" /></a>
+            <a href="#">
+              <img src="pic/XC40.jpeg" alt="XC40" />
+            </a>
           </li>
           <li>
             <p>XC90 建議售價$2,895,000起</p>
-            <a href="#"><img src="pic/XC90.jpeg" alt="XC90" /></a>
+            <a href="#">
+              <img src="pic/XC90.jpeg" alt="XC90" />
+            </a>
           </li>
         </ul>
         <button onClick={moreContent}>Call to Action</button>
         <Footer />
       </div>
     </main>
-  )
+  );
 }
-
 
 function Footer() {
   return (
@@ -94,24 +117,32 @@ function Footer() {
         <ul className="content-list">
           <li>
             <p>Content Box 5</p>
-            <a href="#"><img src="pic/xc60-recharge-gallery-1-16x9.jpeg" alt="S60" /></a>
+            <a href="#">
+              <img src="pic/xc60-recharge-gallery-1-16x9.jpeg" alt="S60" />
+            </a>
           </li>
           <li>
             <p>Content Box 6</p>
-            <a href="#"><img src="pic/xc60-recharge-gallery-3-16x9.jpeg" alt="S90" /></a>
+            <a href="#">
+              <img src="pic/xc60-recharge-gallery-3-16x9.jpeg" alt="S90" />
+            </a>
           </li>
           <li>
             <p>Content Box 7</p>
-            <a href="#"><img src="pic/xc60-recharge-gallery-4-16x9.jpeg" alt="XC40" /></a>
+            <a href="#">
+              <img src="pic/xc60-recharge-gallery-4-16x9.jpeg" alt="XC40" />
+            </a>
           </li>
           <li>
             <p>Content Box 8</p>
-            <a href="#"><img src="pic/xc60-recharge-gallery-5-16x9.jpeg" alt="XC90" /></a>
+            <a href="#">
+              <img src="pic/xc60-recharge-gallery-5-16x9.jpeg" alt="XC90" />
+            </a>
           </li>
         </ul>
       </div>
     </footer>
-  )
+  );
 }
 
 function App() {
@@ -120,17 +151,7 @@ function App() {
       <Header />
       <Main />
     </div>
-  )
+  );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-
-
-
-
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
